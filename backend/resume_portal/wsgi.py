235b12e9a2bb8c70adfd69f1c1f18ad39b22ba4e
justsarx <sinhasarthak56@gmail.com>
2025-03-04 20:24:04 +0530

    Runs migrations on application startup
    Ensures database migrations are applied automatically when the application starts.
    
    This prevents issues caused by outdated database schemas and simplifies deployment.
    It also includes error handling to log any migration failures.

--- backend/resume_portal/wsgi.py
+++ backend/resume_portal/wsgi.py
@@ -7,9 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
+import django
+import logging
 import os
-
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
+
+try:
+    django.setup()
+    call_command('migrate', interactive=False)
+except Exception as e:
+    logging.error("Error during migration: %s", e)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resume_portal.settings')
 
