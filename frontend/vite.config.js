9075b35ac5ba3f9796d5aca770a5d6e7d14d9d2a
Sarthak <sinhasarthak56@gmail.com>
2025-04-19 17:43:31 +0530

    Enhance Vite configuration for improved build process
    - Added path resolution for the main input file and component aliases in vite.config.js to streamline the build process.
    - Updated server configuration to support history API fallback for React Router.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,10 +16,21 @@ export default defineConfig({
   server: {
     historyApiFallback: true, // Redirects to `index.html` for React Router
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+    commonjsOptions: {
+      include: [/node_modules/],
+    },
+  },
   resolve: {
     extensions: ['.js', '.jsx', '.json'],
     alias: {
-      '@': '/src',
+      '@': resolve(__dirname, './src'),
+      'components': resolve(__dirname, './src/components'),
     }
   }
 })
